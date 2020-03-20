a0f043cd582ebf93603ab69234e2e869bfab95d7
Abhishek Shah <36013070+abhishek21shah@users.noreply.github.com>
2020-03-20 01:18:45 -0400

    Create Lab2.m

---
+++ Lab2/Lab2.m
@@ -0,0 +1,112 @@
+clear all
+close all
+%% Section 2: Model Estimation - 1D case
+% Load in data from 1st workspace
+data1 = load('lab2_1.mat');
+data_a = data1.a;
+data_b = data1.b;
+
+m_a = 5;
+v_a = 1; % We are given std dev is 1, but std_dev^2 = var and 1^2 = 1 
+l_b = 1;
+xA = linspace(min(data_a),max(data_a),length(data_a)); %generates 100 evenly spaced points from min to max of data_a
+xB = linspace(min(data_b),max(data_b),length(data_b));
+
+% Section 2.1: Parametric - Gaussian
+[mean_a, mean_b, var_a, var_b] = OneD_paraGaus(data1);
+pdf_True_a = OneD_NormalDist(m_a, v_a, xA);
+pdf_True_b = OneD_ExponenDist(l_b, xB);
+% Data Set A
+pdf_Gaus_a = OneD_NormalDist(mean_a, var_a, xA);
+figure(1);
+plot(xA, pdf_True_a, 'b');
+hold on;
+plot(xA, pdf_Gaus_a, 'black');
+title('1D Parametric - Gaussian for Data Set A')
+legend('True p(x)','Estimated p(x)')
+
+% Data Set B
+pdf_Gaus_b = OneD_NormalDist(mean_b, var_b, xB);
+figure(2);
+plot(xB, pdf_True_b, 'b');
+hold on;
+plot(xB, pdf_Gaus_b, 'black');
+title('1D Parametric - Gaussian for Data Set B')
+legend('True p(x)','Estimated p(x)')
+
+% Section 2.2: Parametric - Exponential
+% Data Set A
+lambda_a = OneD_calcLambda(data_a);
+pdf_Exp_a = OneD_ExponenDist(lambda_a, xA);
+figure(3);
+plot(xA, pdf_True_a, 'b');
+hold on;
+plot(xA, pdf_Exp_a, 'black');
+title('1D Parametric - Exponential for Data Set A')
+legend('True p(x)','Estimated p(x)')
+
+% Data Set B
+lambda_b = OneD_calcLambda(data_b);
+pdf_Exp_b = OneD_ExponenDist(lambda_b, xB);
+figure(4);
+plot(xB, pdf_True_b, 'b');
+hold on;
+plot(xB, pdf_Exp_b, 'black');
+title('1D Parametric - Exponential for Data Set B')
+legend('True p(x)','Estimated p(x)')
+
+% Section 2.3: Parametric - Uniform
+% Data Set A
+[min_a, max_a] = OneD_uniParam(xA);
+pdf_Uni_a = OneD_UniformDist(min_a, max_a, xA);
+figure(5);
+plot(xA, pdf_True_a, 'b');
+hold on;
+plot(xA, pdf_Uni_a, 'black');
+title('1D Parametric - Uniform for Data Set A')
+legend('True p(x)','Estimated p(x)')
+
+% Data Set B
+[min_b, max_b] = OneD_uniParam(xB);
+pdf_Uni_b = OneD_UniformDist(min_b, max_b, xB);
+figure(6);
+plot(xB, pdf_True_b, 'b');
+hold on;
+plot(xB, pdf_Uni_b, 'black');
+title('1D Parametric - Uniform for Data Set B')
+legend('True p(x)','Estimated p(x)')
+
+% Section 2.4: Non-parametric
+std_dev1 = 0.1;
+std_dev2 = 0.4;
+% Data Set A
+parzen_a_sd1 = OneD_ParzenWindowEst(std_dev1, data_a, xA);
+parzen_a_sd2 = OneD_ParzenWindowEst(std_dev2, data_a, xA);
+figure(7);
+plot(xA, pdf_True_a, 'b');
+hold on;
+plot(xA, parzen_a_sd1, 'black');
+hold on;
+plot(xA, parzen_a_sd2, 'red');
+title('Non Parametric - Parzen for Data Set A')
+legend('True p(x)','Parzen with std dev = 0.1', 'Parzen with std dev = 0.4')
+
+% Data Set B
+parzen_b_sd1 = OneD_ParzenWindowEst(std_dev1, data_b, xB);
+parzen_b_sd2 = OneD_ParzenWindowEst(std_dev2, data_b, xB);
+figure(8);
+plot(xB, pdf_True_b, 'b');
+hold on;
+plot(xB, parzen_b_sd1, 'black');
+hold on;
+plot(xB, parzen_b_sd2, 'red');
+title('Non Parametric - Parzen for Data Set B')
+legend('True p(x)','Parzen with std dev = 0.1', 'Parzen with std dev = 0.4')
+
+%% Section 3: Model Estimation - 2D Case
+load('lab2_2.mat');
+%Section 3.1: Parametric
+
+%Section 3.2: 
+%% Section 4: Sequential Determinants
+load('lab2_3.mat'); % 
