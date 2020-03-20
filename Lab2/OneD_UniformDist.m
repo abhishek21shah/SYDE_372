831cd10f31a1211c3be19f80e8f758d31a9c33b3
Abhishek Shah <36013070+abhishek21shah@users.noreply.github.com>
2020-03-20 01:21:05 -0400

    Update OneD_UniformDist.m

--- Lab2/OneD_UniformDist.m
+++ Lab2/OneD_UniformDist.m
@@ -1,6 +1,5 @@
 function [pdf] = OneD_UniformDist(min_,max_, data)
-%ONED_UNIFORMDIST Summary of this function goes here
-%   Detailed explanation goes here
+
 for x = 1:length(data)
     pdf(x) = 1/(max_ - min_);
 end
