d40d033f18f336302a9abd600f319de8ad7b91d5
Ahad Khan <ahad786@live.ca>
2020-03-24 21:13:17 -0400

    Update Lab2.m

--- Lab2/Lab2.m
+++ Lab2/Lab2.m
@@ -119,7 +119,34 @@ legend('True p(x)','Parzen with std dev = 0.4')
 clear all; %may need to remove this 
 close all; %ditto
 load('lab2_2.mat');
+
 %Section 3.1: Parametric Estimation
+%Find mean and Covariances of each class 
+meanA= meanCalc(al); 
+meanB= meanCalc(bl); 
+meanC= meanCalc(cl); 
+covA= covCalc(al); 
+covB= covCalc(bl);
+covC= covCalc(cl);
+
+%Create meshgrid 
+minX= min([al(:,1);bl(:,1);cl(:,1)])-1; 
+maxX= max([al(:,1);bl(:,1);cl(:,1)])+1;
+minY= min([al(:,2);bl(:,2);cl(:,2)])-1;
+maxY= max([al(:,2);bl(:,2);cl(:,2)])+1;
+
+x= minX:1:maxX; 
+y= minY:1:maxY; 
+[x1,y1]= meshgrid(x,y); 
+
+%Get components of graph 
+ML_ab= getMap(covA,covB,meanA',meanB',x1,y1); 
+ML_ac= getMap(covA,covC,meanA',meanC',x1,y1); 
+ML_bc= getMap(covB,covC,meanB',meanC',x1,y1); 
+
+%Plot the results
+Plot = ML_Classification(x1,y1,ML_ab,ML_ac,ML_bc);
+Plot_Param_2d(x1, y1, Plot, at, bt, ct);
 
 %Section 3.2: Non-Parametric Estimation
 %Gaussian window, variance of 400
